abad497f0e028aef30050f98a9f19c9ac80f0d8d
ujjwalsotra <69038984+ujjwalsotra@users.noreply.github.com>
2022-01-06 00:21:34 +0530

    show and hide modal window.

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 'use strict';
+
+const modal=document.querySelector('.modal');
+const overlay=document.querySelector('.overlay');
+const close=document.querySelector('.close-modal');
+const btnOpen=document.querySelectorAll('.show-modal');
+
+const closedModal=function(){
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
+const OpenModal=function()
+{
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
+
+for(let i=0;i<btnOpen.length;i++)
+{
+    btnOpen[i].addEventListener('click',OpenModal);
+}
+close.addEventListener('click',closedModal);
+
+// close the modal window when we click outside of the modal
+
+overlay.addEventListener('click',closedModal);
